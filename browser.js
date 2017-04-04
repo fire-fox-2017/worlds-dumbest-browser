d53a106d9e2dd12fc0b4990c21050b44f0038d10
akbarsahata <akbarsahata@outlook.co.id>
2017-04-04 09:32:31 +0700

    Template

---
+++ browser.js
@@ -0,0 +1,23 @@
+"use strict"
+
+var http = require('http')
+var cheerio = require('cheerio')
+
+var util = require('./util')
+
+class Browser {
+  run() {
+    // Run the browser
+    // Display a prompt for a user
+    // Parse their input
+    // Display useful results by instantiating
+    //   a new Page and calling methods on it.
+    //
+    // Questions:
+    //  1. How can a user quit the browser gracefully?
+    //  2. How can a user ask for help, i.e., how do they know what commands are available to them?
+  }
+}
+
+var browser = new Browser
+browser.run()
---
+++ util.js
@@ -0,0 +1,21 @@
+"use strict"
+
+class Page {
+  constructor(url) {
+  }
+  
+  fetch() {
+  }
+  
+  title() {
+  }
+  
+  links() {
+    // Research alert!
+    // How do you use Nokogiri to extract all the link URLs on a page?
+    //
+    // These should only be URLs that look like
+    //   <a href="http://somesite.com/page.html">Click here!</a>
+    // This would pull out "http://somesite.com/page.html"
+  }
+}
